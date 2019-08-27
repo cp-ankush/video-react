4d467b9164a32e53ee1ef851e22550cf2e86c37c
ankush-l <ankush@licious.in>
2019-08-27 16:59:47 +0530

    play the video

--- src/video/index.jsx
+++ src/video/index.jsx
@@ -1,12 +1,11 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import './styles.css'
 
 const VideoPlayer = ({ src, type, children }) => {
-  const inputEl = useRef(null);
   return (
     <div className="video-container">
-      <video width="35%" height="49%" ref={inputEl}>
+      <video width="35%" height="49%" autoPlay>
         <source
           src={src}
           type={type}
